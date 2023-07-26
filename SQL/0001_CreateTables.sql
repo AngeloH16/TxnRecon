b4cf76ef454ba097fb19463e1ae8eb9210c79de8
angelo.hastings@outlook.com <angelo.hastings93@gmail.com>
2023-07-26 21:21:43 +1000

    update columns for diag data

--- SQL/0001_CreateTables.sql
+++ SQL/0001_CreateTables.sql
@@ -1,5 +1,7 @@
 USE [TxnRecon]
-IF OBJECT_ID('raw_txns', N'U') IS NULL
+IF OBJECT_ID('raw_txns', N'U') IS NOT NULL
+DROP TABLE raw_txns
+GO
 CREATE TABLE raw_txns (
      BSBNumber          nvarchar(max)
     ,AccountNumber      nvarchar(max)
@@ -10,12 +12,15 @@ CREATE TABLE raw_txns (
     ,Credit             nvarchar(max)
     ,Balance            nvarchar(max)
     ,TransactionType    nvarchar(max)
+    ,SourceFile         nvarchar(max)
     );
 GO
-
-IF OBJECT_ID('std_txns', N'U') IS NULL
-CREATE TABLE std_txns (
-     BSBNumber          nvarchar(max)
+IF OBJECT_ID('stg_txns', N'U') IS NOT NULL
+DROP TABLE stg_txns
+GO
+CREATE TABLE stg_txns (
+     STG_ID                 int identity(1,1) PRIMARY KEY
+    ,BSBNumber          nvarchar(max)
     ,AccountNumber      nvarchar(max)
     ,TransactionDate    date
     ,Narration          nvarchar(max)
@@ -24,5 +29,25 @@ CREATE TABLE std_txns (
     ,Credit             money
     ,Balance            money
     ,TransactionType    nvarchar(max)
+    ,SourceFile         nvarchar(max)
+    ,FileRank           int
+    );
+GO
+IF OBJECT_ID('std_txns', N'U') IS NOT NULL
+DROP TABLE std_txns
+GO
+CREATE TABLE std_txns (
+     ID                 int identity(1,1) PRIMARY KEY
+    ,BSBNumber          nvarchar(max)
+    ,AccountNumber      nvarchar(max)
+    ,TransactionDate    date
+    ,Narration          nvarchar(max)
+    ,Cheque             money
+    ,Debit              money
+    ,Credit             money
+    ,Balance            money
+    ,TransactionType    nvarchar(max)
+    ,load_dt            datetime
+    ,SourceFile         nvarchar(max)
     );
 GO
